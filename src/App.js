7efb0e2e5d80e5e9d914b3d6da4b83ddae433e06
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2024-05-06 20:45:51 -0400

    Made buttons that do other things

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function buttonDoStuff(){
-  alert("I can make a message! So cool.");
+  alert("I can make a message! So cool :)");
 }
+
+function newButton(){
+  alert("WOOOOOOOOOOOOOOOOOOH! :0");
+}
+//Ternary operator refresh x ? y : z
+// if x is true, y       if x is false, z
 
 function App() {
   return (
@@ -12,6 +18,7 @@ function App() {
         <h1>HI THERE!</h1>
         <p>figuring out how this works (React, that is)</p>
         <button onClick={buttonDoStuff}>Click Me! I do stuff.</button>
+        <button onClick={newButton}>Howdy, I'm a brand new button. What do I do? Click to find out.</button>
       </header>
     </div>
   );
